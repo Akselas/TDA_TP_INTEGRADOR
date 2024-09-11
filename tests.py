26b2e88bf196665c63696478461b36ae47349ddf
Axel <67176289+Akselas@users.noreply.github.com>
2024-09-10 20:33:54 -0400

    Added: Test de numeros grandes

--- tests.py
+++ tests.py
@@ -2,14 +2,37 @@ from TP1_juego_monedas import juego_monedas
 from jugador import Jugador
 
 def pruebas():
+    test_comun()
+    test_numeros_grandes()
 
+def test_comun():
+    "Arrange"
     arr = [3,9,1,8,2,5]
     sofia = Jugador("Sofia")
     mateo = Jugador("Mateo")
     turno = True
+    
+    "Act"
     juego_monedas(arr, turno, sofia, mateo)
-    assert sofia.monedero == 22, "No devuelve la correcta cantidad de monedas, sofia tiene: {sofia.monedero} monedas"
-    assert mateo.monedero == 6, "No devuelve la correcta cantidad de monedas, mateo tiene: {mateo.monedero} monedas"
-    print("Tests Pasaron")
+    
+    "Assert"
+    assert sofia.monedero == 22, "No devuelve la correcta cantidad de monedas, sofia tiene: {sofia.monedero} pesos"
+    assert mateo.monedero == 6, "No devuelve la correcta cantidad de monedas, mateo tiene: {mateo.monedero} pesos"
+    print("Test comun paso")
+
+def test_numeros_grandes():
+    "Arrange"
+    arr = [413759, 698681, 730491, 596380, 3014, 416704, 874886, 35898, 686505, 491032]
+    sofia = Jugador("Sofia")
+    mateo = Jugador("Mateo")
+    turno = True
+    "Act"
+    juego_monedas(arr, turno, sofia, mateo)
+    
+    "Assert"
+    assert sofia.monedero == 3391470, "No devuelve la correcta cantidad de monedas, sofia tiene: {sofia.monedero} pesos"
+    assert mateo.monedero == 1555880, "No devuelve la correcta cantidad de monedas, mateo tiene: {mateo.monedero} pesos"
+    print("Tests con numeros grandes paso")
+
 
 pruebas()
